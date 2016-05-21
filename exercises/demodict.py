a3a74f4289a331bd74cb3e8a1e9332a611e3e2ec
wangdegang <dgwangh@yahoo.com>
2016-05-21 18:43:27 -0500

    done variables.py

--- exercises/demodict.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-
-def generate_dict():
-    keys = ["hello", "there", "hello_there", "hello there", "dictionaries are cool"]
-    return {key: hashlib.sha224(key).hexdigest() for key in keys}
